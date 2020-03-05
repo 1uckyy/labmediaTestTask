06e372029544d276af03115be7857c2c72154114
Vladislav Novak <novak.vlad97@mail.ru>
2020-03-06 00:31:18 +0300

    Fixed modal wnd markup and styles.

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@
 $(function() {
     $(".choose_employee_btn").click(function(){
         $('body').append('<div class="modal"></div>');
-        $('.modal').append('<div class="title_modal">Заголовок</div>');
+        $('.modal').append('<div class="head_modal">Заголовок</div>');
+        $('.head_modal').append('<button class="close_btn_modal">X</button>');
         $('.modal').append('<div class="choose_container_modal"></div>');
-        $('.modal').append('<button class="close_btn_modal">X</button>');
-        $('.modal').append('<button class="ok_btn_modal">ОК</button>');
-        $('.modal').append('<button class="cancel_btn_modal">Отмена</button>');
+        $('.modal').append('<div class="controls_modal"></div>');
+        $('.controls_modal').append('<div class="controls_inter_modal"></div>');
+        $('.controls_inter_modal').append('<button class="btn_modal">ОК</button>');
+        $('.controls_inter_modal').append('<button class="btn_modal">Отмена</button>');
 
         $('body').append('<div class="gray_screen_modal"></div>');
     });
