fe42ea2852a4b014ee6412ba3a775073b42a8ecf
Vladislav Novak <novak.vlad97@mail.ru>
2020-03-06 11:47:45 +0300

    Added choose opportunity.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 "use strict"
+var choices_ids = [];
+
 $(function() {
     $(".choose_employee_btn").click(function(){
         //modal wnd markup
@@ -9,8 +11,8 @@ $(function() {
         $('.choose_container_modal').append('<table class="choose_table_modal"></table>');
         $('.modal').append('<div class="controls_modal"></div>');
         $('.controls_modal').append('<div class="controls_inter_modal"></div>');
-        $('.controls_inter_modal').append('<button class="btn_modal">ОК</button>');
-        $('.controls_inter_modal').append('<button class="btn_modal">Отмена</button>');
+        $('.controls_inter_modal').append('<button class="btn_modal ok_btn_modal">ОК</button>');
+        $('.controls_inter_modal').append('<button class="btn_modal cancel_btn_modal">Отмена</button>');
 
         $('body').append('<div class="gray_screen_modal"></div>');
 
@@ -28,12 +30,50 @@ $(function() {
             });
 
             //fill modal
-            for(var i=0; i<persons.length; i++) {
-                $('.choose_table_modal').append('<tr><td>' + persons[i].lastname + '</td><td>' + persons[i].middlename + '</td><td>' + persons[i].firstname + '</td><td>' + persons[i].birthday + '</td></tr>');
+            for(let i=0; i<persons.length; i++) {
+                $('.choose_table_modal').append('<tr id=' + i + '><td>' + persons[i].lastname + '</td><td>' + persons[i].middlename + '</td><td>' + persons[i].firstname + '</td><td>' + persons[i].birthday + '</td></tr>');
             }
+
+            //gray bg-clr choosed persons
+            for(let i = 0; i<choices_ids.length; i++) {
+                $('.choose_table_modal > #' + choices_ids[i]).css('background-color', 'lightgreen');
+            }
+            
+            var prev_id = null;
+            var clicked_id = null;
+            //choose row
+            $('.choose_table_modal > tr').click(function(){
+                clicked_id = $(this).attr('id');
+                if(!choices_ids.find(function(elem){return elem==clicked_id})) {
+                    $('.choose_table_modal > #' + clicked_id).css('background-color', 'gray');
+
+                    if(prev_id != null) {
+                        $('.choose_table_modal > #' + prev_id).css('background-color', 'white');
+                    }
+
+                    prev_id = clicked_id;
+                }
+            });
+
+            //ok btn
+            $(".ok_btn_modal").click(function(){
+                if(clicked_id != null && !choices_ids.find(function(elem){return elem==clicked_id})) {
+                    $('.employee_selection_cell').append('<div><div class="text_selection">' + persons[clicked_id].lastname + ' ' + persons[clicked_id].middlename + ' ' + persons[clicked_id].firstname + '</div><button class="btn_X asd">X</button><div class="hidden_index">' + choices_ids.length + '</div></div>');
+                    choices_ids.push(clicked_id);
+                }
+                $('.modal').remove();
+                $('.gray_screen_modal').remove();
+            });
         });
 
+        //close btn
         $(".close_btn_modal").click(function(){
+            $('.modal').remove();
+            $('.gray_screen_modal').remove();
+        });
+
+        //cancel btn
+        $(".cancel_btn_modal").click(function(){
             $('.modal').remove();
             $('.gray_screen_modal').remove();
         });
